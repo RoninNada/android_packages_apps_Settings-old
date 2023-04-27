xNombre/android_packages_apps_Settings

Implement PIE 3.0 [2/3]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -137,6 +137,7 @@ import com.crdroid.settings.crDroidSettings;
 import com.crdroid.settings.crDroidSettingsLayout;
 import com.crdroid.settings.fragments.NavBar;
 import com.crdroid.settings.fragments.navbar.Fling;
+import com.crdroid.settings.fragments.navbar.PieSettings;
 import com.crdroid.settings.fragments.navbar.Pulse;
 import com.crdroid.settings.fragments.navbar.Smartbar;
 
@@ -268,6 +269,7 @@ public class SettingsGateway {
             Pulse.class.getName(),
             Smartbar.class.getName(),
             RunningServices.class.getName(),
+            PieSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -313,5 +315,6 @@ public class SettingsGateway {
             Settings.PulseSettingsActivity.class.getName(),
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
+            Settings.PieControlSettingsActivity.class.getName(),
     };
 }

RoninNada/android_packages_apps_Settings-old

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -135,6 +135,7 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.custom.settings.CustomSettings;
 import com.custom.settings.fragments.NavBar;
 import com.custom.settings.navbar.Fling;
+import com.custom.settings.fragments.navbar.PieSettings;
 import com.custom.settings.navbar.Pulse;
 import com.custom.settings.navbar.Smartbar;
 